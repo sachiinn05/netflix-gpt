704c0abb85912e45ed14406addfe68c0f65c191d
sachinsingh <sachinsingh6386@gmail.com>
2025-10-28 14:04:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-green-400">
+      <p>Lets build netfilx built</p>
+    </div>
+  );
+}
+
+export default App;
