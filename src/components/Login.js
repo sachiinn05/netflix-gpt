973ad774ca24ac2e0ea11be8cb8bd9b7f3997f29
sachinsingh <sachinsingh6386@gmail.com>
2025-10-29 12:59:02 +0530

    SignIn and SignUp

---
+++ src/components/Login.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import Header from './Header'
+
+const Login = () => {
+  const [isSignInForm,setIsSignInFrom]=useState(true);
+  const toggleSignUpForm=()=>{
+     setIsSignInFrom(!isSignInForm);
+  }
+  return (
+    <div>
+      <Header/>
+      <div className='absolute'>
+        <img 
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/9ba9f0e2-b246-47f4-bd1f-3e84c23a5db8/web/IN-en-20251020-TRIFECTA-perspective_d6da84e9-6145-4b1e-bb51-e402c966a045_small.jpg"
+        alt="background"/>
+      </div>
+
+      <form className="w-3/12 min-w-[300px] absolute p-12 bg-black/80 my-40 mx-auto right-0 left-0 text-white rounded-md">
+      <h1 className="font-bold text-3xl py-4">
+        {isSignInForm? "Sign In" :"Sign Up"}</h1>
+      { !isSignInForm && (
+     <input
+      type="text"
+       placeholder="Full Name"
+      className="p-4 my-3 w-full bg-[#333] rounded text-white placeholder-gray-400 focus:outline-none"
+     />
+     )}
+      <input
+      type="email"
+      placeholder="Email or phone number"
+      className="p-4 my-3 w-full bg-[#333] rounded text-white placeholder-gray-400 focus:outline-none"
+      />
+      <input
+      type="password"
+       placeholder="Password"
+      className="p-4 my-3 w-full bg-[#333] rounded text-white placeholder-gray-400 focus:outline-none"
+     />
+
+    
+
+     <button className="p-4 my-6 bg-red-700 hover:bg-red-800 w-full rounded font-semibold">
+      {isSignInForm? "Sign In" :"Sign Up"}
+     </button>
+       <div className="flex justify-between text-sm text-gray-400">
+  </div>
+     <p
+  className="py-4 text-gray-400 text-sm cursor-pointer transition-all duration-300 hover:underline hover:text-white"
+  onClick={toggleSignUpForm}
+>
+  {isSignInForm
+    ? "New to Netflix? "
+    : "Already registered? "}
+  <span className="text-white font-semibold">
+    {isSignInForm ? "Sign up now." : "Sign in now."}
+  </span>
+</p>
+
+</form>
+
+    </div>
+  )
+}
+
+export default Login
